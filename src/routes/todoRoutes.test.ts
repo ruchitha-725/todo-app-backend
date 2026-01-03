61dc8713648c0479d9db9bb898c5116d56f6ce2a
Ruchitha Reddy <ruchitha.bondala@everest.engineering>
2026-01-03 12:17:31 +0530

    Update route test file with view task path tests

--- src/routes/todoRoutes.test.ts
+++ src/routes/todoRoutes.test.ts
@@ -1,20 +1,53 @@
-import request from "supertest";
-import express from "express";
-import todoRoutes from "./todoRoutes";
-import addTasksController from "../controllers/todoListControllers";
+import request from 'supertest';
+import express, { Express } from 'express';
+import todoRoutes from './todoRoutes';
+import { addTasksController,viewTasksController } from '../controllers/todoListControllers';
 
-jest.mock("../controllers/todoListControllers", () => jest.fn((req, res) => res.status(200).send()));
+jest.mock('../controllers/todoListControllers', () => ({
+  addTasksController: jest.fn((req, res) => {
+    res.status(200).json({ status: 'controller function called' });
+  }),
+  viewTasksController: jest.fn((req, res) => {
+    res.status(200).json({ status: 'getTasks controller called' });
+  }),
+}));
 
-const app = express();
+const mockedAddTask = addTasksController as jest.Mock;
+const mockedGetTasks = viewTasksController as jest.Mock;
+
+const app: Express = express();
 app.use(express.json());
-app.use("/todoTasks", todoRoutes); 
+app.use('/todoTasks', todoRoutes);
 
-describe("Todo Routes", () => {
-  it("should call addTasksController", async () => {
-    const response = await request(app)
-      .post("/todoTasks/add")
-      .send({ name: "Yoga" });
-    expect(response.status).toBe(200);
-    expect(addTasksController).toHaveBeenCalled();
+describe('todoRoutes', () => {
+  beforeEach(() => {
+    jest.clearAllMocks();
   });
+
+  it('should call the addTask controller function when POST is requested', async () => {
+    const testBody = { name: "Yoga" };
+    const response = await request(app)
+      .post('/todoTasks/add')
+      .send(testBody)
+      .expect(200);
+    expect(mockedAddTask).toHaveBeenCalledTimes(1);
+    expect(response.body).toEqual({ status: 'controller function called' });
+    const reqArg = mockedAddTask.mock.calls[0][0];
+    expect(reqArg.body).toEqual(testBody);
+  });
+  it('should return 404 for a route that does not exist within the router', async () => {
+    await request(app)
+      .get('/todoTasks/adds')
+      .expect(404);
+    expect(mockedAddTask).not.toHaveBeenCalled();
+  });
+  it('should call the getTasks controller function when GET is requested', async () => {
+    const response = await request(app)
+      .get('/todoTasks/view')
+      .expect(200);
+    expect(mockedGetTasks).toHaveBeenCalledTimes(1);
+    expect(mockedAddTask).not.toHaveBeenCalled();
+    expect(response.body).toEqual({ status: 'getTasks controller called' });
+  }); 
+  
 });
