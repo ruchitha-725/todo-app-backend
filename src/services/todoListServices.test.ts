a28801c5267936fc920ce8d25f64ca135355a960
Ruchitha Reddy <ruchitha.bondala@everest.engineering>
2026-01-03 12:12:55 +0530

    Update todoList Service test file with viewTasks Service tests

--- src/services/todoListServices.test.ts
+++ src/services/todoListServices.test.ts
@@ -1,4 +1,4 @@
-import addTasksService from "./todoListServices";
+import {addTasksService,viewTasksService} from "./todoListServices";
 import { db } from "../firebaseConfiguration/firebase";
 import { taskStatus, taskPriority } from "../types/taskType";
 
@@ -77,3 +77,66 @@ describe("addTasksService", () => {
   });
 
 });
+
+describe("viewTasksService", () => {
+  afterEach(() => {
+    jest.clearAllMocks();
+  });
+  it("should return an empty array if there are no tasks", async () => {
+    (db.collection as jest.Mock).mockReturnValue({
+      get: jest.fn().mockResolvedValue({ empty: true }),
+    });
+    const result = await viewTasksService();
+    expect(result).toEqual([]);
+  });
+    it("should return a list of tasks when the database has data", async () => {
+    const mockDocs = [
+      {
+        id: "id123",
+        data: () => ({ 
+          name: "Yoga",
+          description: "Exercise", 
+          deadline: "2026-01-11",
+          Status: taskStatus.IN_PROGRESS,
+          Priority: taskPriority.MEDIUM 
+        }),
+      },
+      {
+        id: "id234",
+        data: () => ({ 
+          name: "Coding",
+          description: "Assignment", 
+          deadline: "2026-02-15", 
+          Status: taskStatus.PENDING,
+          Priority: taskPriority.HIGH
+        }),
+      },
+    ];
+    (db.collection as jest.Mock).mockReturnValue({
+      get: jest.fn().mockResolvedValue({
+        empty: false,
+        docs: mockDocs,
+      }),
+    });
+    const result = await viewTasksService();
+    expect(result).toHaveLength(2);
+    expect(result[0]).toEqual({ 
+      id: "id123", 
+      name: "Yoga", 
+      description: "Exercise", 
+      deadline: "2026-01-11",
+      Status: taskStatus.IN_PROGRESS,
+      Priority: taskPriority.MEDIUM 
+    });
+    expect(result[1].name).toBe("Coding");
+  });
+  it("should throw an error message if the database fails", async () => {
+    (db.collection as jest.Mock).mockReturnValue({
+      get: jest.fn().mockRejectedValue(new Error("Firebase connection lost")),
+    });
+    await expect(viewTasksService()).rejects.toThrow(
+      "Failed to retrieve the tasks from database"
+    );
+  });
+});
+
