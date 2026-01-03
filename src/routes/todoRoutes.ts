de52d60254b6bb9f7a528da1e3c44fad366eb2d6
Ruchitha Reddy <ruchitha.bondala@everest.engineering>
2026-01-03 14:32:45 +0530

    Update todoList route file with delete task route

--- src/routes/todoRoutes.ts
+++ src/routes/todoRoutes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import {addTasksController,viewTasksController,editTasksController}  from "../controllers/todoListControllers";
+import {addTasksController,viewTasksController,editTasksController,deleteTasksController}  from "../controllers/todoListControllers";
 
 const todoRoutes = Router();
 
 todoRoutes.post("/add", addTasksController);
 todoRoutes.get("/view",viewTasksController);
-todoRoutes.patch("/editTask",editTasksController)
+todoRoutes.patch("/editTask",editTasksController);
+todoRoutes.delete("/deleteTask/:id", deleteTasksController);
 
 export default todoRoutes;
