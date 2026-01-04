034a0452309782e183fed2264a185aab736f6ef2
Ruchitha Reddy <ruchitha.bondala@everest.engineering>
2026-01-05 01:34:54 +0530

    Update tests of editTask route

--- src/routes/todoRoutes.test.ts
+++ src/routes/todoRoutes.test.ts
@@ -1,7 +1,7 @@
 import request from 'supertest';
 import express, { Express } from 'express';
 import todoRoutes from './todoRoutes';
-import { addTasksController,viewTasksController,editTasksController,deleteTasksController } from '../controllers/todoListControllers';
+import { addTasksController, viewTasksController, editTasksController, deleteTasksController } from '../controllers/todoListControllers';
 
 jest.mock('../controllers/todoListControllers', () => ({
   addTasksController: jest.fn((req, res) => {
@@ -10,11 +10,11 @@ jest.mock('../controllers/todoListControllers', () => ({
   viewTasksController: jest.fn((req, res) => {
     res.status(200).json({ status: 'getTasks controller called' });
   }),
-   editTasksController: jest.fn((req, res) => { 
+  editTasksController: jest.fn((req, res) => {
     res.status(200).json({ status: 'editTasks controller called' });
   }),
-  deleteTasksController:jest.fn((req,res) => {
-    res.status(200).json({status:'deleteTask controller called'})
+  deleteTasksController: jest.fn((req, res) => {
+    res.status(200).json({ status: 'deleteTask controller called' })
   })
 }));
 
@@ -31,7 +31,6 @@ describe('todoRoutes', () => {
   beforeEach(() => {
     jest.clearAllMocks();
   });
-
   it('should call the addTask controller function when POST is requested', async () => {
     const testBody = { name: "Yoga" };
     const response = await request(app)
@@ -56,22 +55,22 @@ describe('todoRoutes', () => {
     expect(mockedGetTasks).toHaveBeenCalledTimes(1);
     expect(mockedAddTask).not.toHaveBeenCalled();
     expect(response.body).toEqual({ status: 'getTasks controller called' });
-  }); 
- it('should call the editTasks controller function when PATCH is requested', async () => {
-    const result = { name: "Yoga", description: "Updated" };    
+  });
+  it('should call the editTasks controller function when PATCH is requested', async () => {
+    const result = { name: "Yoga", description: "Updated" };
     const response = await request(app)
-      .patch('/todoTasks/editTask')
-      .send(result)             
+      .patch('/todoTasks/editTask/id123')
+      .send(result)
       .expect(200);
     expect(mockedEditTasks).toHaveBeenCalledTimes(1);
     expect(response.body).toEqual({ status: 'editTasks controller called' });
     const reqArg = mockedEditTasks.mock.calls[0][0];
     expect(reqArg.body).toEqual(result);
-});
-it('should call the deleteTasks controller when DELETE is requested with an ID', async () => {
+  });
+  it('should call the deleteTasks controller when DELETE is requested with an ID', async () => {
     const id = "id12345";
     const response = await request(app)
-      .delete(`/todoTasks/deleteTask/${id}`) 
+      .delete(`/todoTasks/deleteTask/${id}`)
       .expect(200);
     expect(mockedDeleteTask).toHaveBeenCalledTimes(1);
     const reqArg = mockedDeleteTask.mock.calls[0][0];
@@ -81,7 +80,7 @@ it('should call the deleteTasks controller when DELETE is requested with an ID',
   it('should return 404 if the delete URL path is wrong', async () => {
     await request(app)
       .delete('/todoTasks/deleteTasks/id123')
-      .expect(404);      
+      .expect(404);
     expect(mockedDeleteTask).not.toHaveBeenCalled();
   });
 });
